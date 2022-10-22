8112973c71f748349b877d7d5e10fcef4f0f70bd
Marcos Leite <30819867+mabner@users.noreply.github.com>
2022-10-22 01:21:30 -0300

    old code

---
+++ README.md
@@ -0,0 +1,3 @@
+# SenhaToledo
+
+Aplicativo em C# pra gerar a senha master do dia que é utilizada pra configurar as balanças Toledo.
---
+++ Toledo/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Toledo
+{
+    public partial class Form1 : Form
+    {
+        public DateTime thisDay = DateTime.Today;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(thisDay.ToString("dd")) + 20;
+            int n2 = Convert.ToInt32(thisDay.ToString("MM")) + 11;
+            label1.Text = Convert.ToString(n1) + Convert.ToString(n2);
+                
+                
+                //thisDay.ToString("dd");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
